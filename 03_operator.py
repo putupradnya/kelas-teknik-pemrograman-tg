762e4ee70819ec0c0661a9ae0c3e102e93245932
Putu Pradnya <putu.pradnya@divistant.com>
2025-09-08 21:03:51 +0700

    operator dan stuktur data

---
+++ 03_operator.py
@@ -0,0 +1,20 @@
+# operator aritmatika
+x = 5
+y = 2
+z = 10
+
+hasil = x / y
+
+# print("hasil: ", hasil)
+
+
+# operator comparison
+# print(x == y)
+
+
+# operator logic: and, or, not
+# print((x > y) and (z > x))
+# print((x > y) or (z < x))
+
+# contoh not
+print(not(x > y))
---
+++ 04_stuktur_data.py
@@ -0,0 +1,29 @@
+# List
+# list_buah = ["mangga", "apel", "jeruk"]
+# print(list_buah)
+
+# list_buah.append("Duren")
+# print(list_buah)
+
+# print(list_buah[1:3])
+
+# dictionary
+data_mahasiswa = {
+    "nama": ["putu pradnya", "budi", "susi"],
+    "nim": "12115017",
+    "prodi": "teknik geofisika",
+    "fakultas": "FTI"
+    }
+
+# print(data_mahasiswa["nama"][0])
+
+# Tuple
+data_kota = ("bandung", "jakarta", "surabaya", "medan")
+# data_kota.append("palembang")
+# print(data_kota[0])
+
+
+# Set
+data_warna = {"merah", "kuning", "hijau", "biru", "merah"}
+data_warna.
+print(data_warna)
